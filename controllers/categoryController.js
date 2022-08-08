eaf3bc2eca42088c99485b67fddd2f52729d94d3
carlk <carloskachuk@gmail.com>
2022-08-07 22:37:22 -0300

    the category controller was implemented

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,17 +1,69 @@
+var Category = require('../models/category');
+var User = require('../models/user');
+var Post = require('../models/post')
+var {body, validationResult} = require("express-validator");
 
-module.exports.getCategories = (req, res, next)=>{
-    res.json({message: "GET ALL CATEGORIES"})
+exports.getCategories = (req, res, next)=>{
+   Category.find({}, (err, categories)=>{
+    if(err) {return next(err)}
+    res.json({status: "OK", message:"all categories", categories: categories})
+   })
+
 }
 
 
-module.exports.createCategory = (req, res, next)=>{
-    res.json({message: "CREATE CATEGORY"})
+exports.createCategory = [
+    body('name', 'Name does not have to be empty').trim().isLength({min:1}).escape(),
+    async (req, res, next)=>{
+        var errors = validationResult(req.body);
+
+        var category = new Category({
+            name: req.body.name
+        })
+
+        if(!errors.isEmpty()){
+            res.json({status: "FAILED",  message: errors.array()})
+        }else{
+            User.findOne({username:req.body.currentUser})
+            .populate('credential')
+            .exec((err, user)=>{
+                if(err){return next(err)}
+    
+                if(user.credential.isAdmin){
+                    category.save((err, category)=>{
+                        if(err){return next(err)}
+    
+                        res.json({status:"OK", message: "The category has been created", category: category})
+                    })
+                }else{
+                    res.json({status:"FAILED", message: "User does not have the permissions"})
+                }
+            })
+        }
+    }
+]
+
+exports.deleteCategory = (req, res, next)=>{
+    User.findOne({username: req.body.currentUser}) 
+    .populate('credential')
+    .exec( (err, user)=>{
+        if(err){return next(err)}
+
+        if(user.credential.isAdmin){
+            Category.findByIdAndDelete(req.params.id, (err)=>{
+                if(err){return next(err)}
+
+                res.json({status:"OK", message:"The category has been deleted"})
+            })
+        }
+    })
+   
 }
 
-module.exports.deleteCategory = (req, res, next)=>{
-    res.json({message: "DELETE CATEGORY"})
-}
+exports.getAllPostsOfCategory = (req, res, next)=>{
+    Post.find({category: {$conteins: req.params.id}}, (err, posts)=>{
+        if(err){ return next(err)}
 
-module.exports.getAllPostsOfCategory = (req, res, next)=>{
-    res.json({message: "GET ALL POSTS OF CATEGORY"})
+        res.json({status: "OK", message:"All posts of category", posts:posts});
+    })
 }
