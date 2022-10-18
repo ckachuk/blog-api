7cf03ad070c52fa37b8d634f5a8630d89099883a
carlk <carloskachuk@gmail.com>
2022-10-18 08:19:36 -0300

    change port

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
-app.listen(process.env.PORT || 5000 , function () {
+app.listen(5000 , function () {
   console.log(' app listening on port 5000 !');
 });
 
